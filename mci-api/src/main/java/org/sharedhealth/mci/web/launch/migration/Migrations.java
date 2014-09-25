0555cb8150c4d484147a7ec9e4ab3d4a50c68b7d
Roni Saha <roni.cse@gmail.com>
2014-09-25 10:48:27 +0600

    Roni | 0000 | Add different properties for test environment

--- mci-api/src/main/java/org/sharedhealth/mci/web/launch/migration/Migrations.java
+++ mci-api/src/main/java/org/sharedhealth/mci/web/launch/migration/Migrations.java
@@ -2,6 +2,7 @@ package org.sharedhealth.mci.web.launch.migration;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import com.netflix.astyanax.AstyanaxContext;
 import com.netflix.astyanax.Keyspace;
@@ -21,10 +22,19 @@ import static java.lang.System.getenv;
 public class Migrations {
 
     private static final String MUTAGEN_CONNECTION_POOL_NAME = "mciMigrationConnectionPool";
+    private final Map<String, String> env;
+
+    public Migrations() {
+        this(getenv());
+    }
+
+    public Migrations(Map<String, String> env) {
+        this.env = env;
+    }
 
     public void migrate() throws IOException, ConnectionException {
         CassandraMutagen mutagen = new CassandraMutagenImpl();
-        mutagen.initialize(getenv("CASSANDRA_MIGRATIONS_PATH"));
+        mutagen.initialize(env.get("CASSANDRA_MIGRATIONS_PATH"));
         Keyspace keyspace = getKeySpace();
         Plan.Result<Integer> result = mutagen.mutate(keyspace);
         if (result.getException() != null) {
@@ -42,15 +52,15 @@ public class Migrations {
 
     private AstyanaxContext<Keyspace> buildContext() {
         return new AstyanaxContext.Builder()
-                .forKeyspace(getenv("CASSANDRA_KEYSPACE"))
+                .forKeyspace(env.get("CASSANDRA_KEYSPACE"))
                 .withAstyanaxConfiguration(new AstyanaxConfigurationImpl()
-                                .setCqlVersion(getenv("CQL_VERSION"))
-                                .setTargetCassandraVersion(getenv("CASSANDRA_VERSION"))
+                                .setCqlVersion(env.get("CQL_VERSION"))
+                                .setTargetCassandraVersion(env.get("CASSANDRA_VERSION"))
                 )
                 .withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl(MUTAGEN_CONNECTION_POOL_NAME)
-                                .setPort(valueOf(getenv("MUTAGEN_CONNECTION_POOL_PORT")))
+                                .setPort(valueOf(env.get("MUTAGEN_CONNECTION_POOL_PORT")))
                                 .setMaxConnsPerHost(1)
-                                .setSeeds(getenv("MUTAGEN_SEEDS"))
+                                .setSeeds(env.get("MUTAGEN_SEEDS"))
                 )
                 .withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
                 .buildKeyspace(ThriftFamilyFactory.getInstance());
--- mci-api/src/test/java/org/sharedhealth/mci/web/config/EnvironmentMock.java
+++ mci-api/src/test/java/org/sharedhealth/mci/web/config/EnvironmentMock.java
@@ -1,15 +1,19 @@
 package org.sharedhealth.mci.web.config;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
+import org.sharedhealth.mci.web.launch.migration.Migrations;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.mock.env.MockPropertySource;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+
 
 @Configuration
 public class EnvironmentMock implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -18,16 +22,28 @@ public class EnvironmentMock implements ApplicationContextInitializer<Configurab
     public void initialize(ConfigurableApplicationContext applicationContext) {
         MutablePropertySources propertySources = applicationContext.getEnvironment().getPropertySources();
         MockPropertySource mockEnvVars = new MockPropertySource();
+        Map<String, String> env = new HashMap<String, String>();
+
         try {
-            FileInputStream inputStream = new FileInputStream(new File(System.getenv("PATH_TO_CONFIG")));
+            InputStream inputStream = this.getClass().getResourceAsStream("/test.properties");
             Properties properties = new Properties();
             properties.load(inputStream);
+
             for (Object property : properties.keySet()) {
                 mockEnvVars.setProperty(property.toString(), properties.getProperty(property.toString()));
+                env.put(property.toString(), properties.getProperty(property.toString()));
             }
             propertySources.replace(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, mockEnvVars);
         } catch (Exception ignored) {
             System.out.print("Error ignored!");
         }
+
+        try {
+            EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+            new Migrations(env).migrate();
+        } catch (Exception e) {
+            throw new RuntimeException("Error starting embedded server..", e);
+        }
+
     }
 }
