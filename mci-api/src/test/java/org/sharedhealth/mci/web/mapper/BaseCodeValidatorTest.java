a1b2331a9c626675c8561ee4dad90a068a351c60
Roni Saha <roni.cse@gmail.com>
2014-10-16 13:46:01 +0600

    Roni | BSHR-312 | Test case rewritten

---
+++ mci-api/src/test/java/org/sharedhealth/mci/web/mapper/BaseCodeValidatorTest.java
@@ -0,0 +1,31 @@
+package org.sharedhealth.mci.web.mapper;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertEquals;
+
+public abstract class BaseCodeValidatorTest<T> {
+
+    @Autowired
+    private Validator validator;
+
+    public void assertValidValues(String[] values, String property, Class<T> tClass) throws Exception {
+        for (String value : values) {
+            Set<ConstraintViolation<T>> constraintViolations = validator.validateValue(tClass, property, value);
+            assertEquals(0, constraintViolations.size());
+        }
+    }
+
+    public void assertInvalidValues(String[] inValidValues, String property, Class<T> tClass) throws Exception {
+
+        for (String relation : inValidValues) {
+            Set<ConstraintViolation<T>> constraintViolations = validator.validateValue(tClass, property, relation);
+            assertEquals(1, constraintViolations.size());
+            assertEquals("1004", constraintViolations.iterator().next().getMessage());
+        }
+    }
+}
