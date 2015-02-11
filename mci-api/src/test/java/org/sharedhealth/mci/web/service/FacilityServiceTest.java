417abdfcd44a107fc420a16945f42594f13c147e
Roni Saha <roni.cse@gmail.com>
2015-02-11 13:33:28 +0600

    Roni | BSHR-502 | Add more test coverage

--- mci-api/src/test/java/org/sharedhealth/mci/web/service/FacilityServiceTest.java
+++ mci-api/src/test/java/org/sharedhealth/mci/web/service/FacilityServiceTest.java
@@ -1,16 +1,23 @@
 package org.sharedhealth.mci.web.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.sharedhealth.mci.web.exception.FacilityNotFoundException;
 import org.sharedhealth.mci.web.infrastructure.fr.FacilityRegistryWrapper;
 import org.sharedhealth.mci.web.infrastructure.persistence.FacilityRepository;
+import org.sharedhealth.mci.web.mapper.Catchment;
 import org.sharedhealth.mci.web.model.Facility;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.never;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -45,9 +52,53 @@ public class FacilityServiceTest {
             InterruptedException {
         Facility facility = new Facility("1", "foo", "bar", "101010", "101010");
 
-        Mockito.when(facilityRepository.find(facility.getId())).thenReturn(facility);
+        Mockito.when(facilityRepository.find(facility.getId())).thenReturn(null);
         Mockito.when(facilityRegistryWrapper.find(facility.getId())).thenReturn(facility);
         assertNotNull(facilityService.ensurePresent(facility.getId()));
+        Mockito.verify(facilityRepository).find(facility.getId());
+        Mockito.verify(facilityRegistryWrapper).find(facility.getId());
+    }
 
+    @Test
+    public void shouldReturnCatchmentLists() throws ExecutionException,
+            InterruptedException {
+
+        final String catchmentsString = "101010,101020";
+
+        Facility facility = new Facility("1", "foo", "bar", catchmentsString, "101010");
+        Mockito.when(facilityRepository.find(facility.getId())).thenReturn(facility);
+        final List<Catchment> catchments = facilityService.getCatchmentAreasByFacility(facility.getId());
+
+        Mockito.verify(facilityRegistryWrapper, never()).find(facility.getId());
+        assertEquals(getCatchmentsList(catchmentsString), catchments);
+    }
+
+    @Test(expected = FacilityNotFoundException.class)
+    public void shouldThroughFacilityNotFoundExceptionIfFacilityNotFound() throws ExecutionException,
+            InterruptedException {
+
+        final String facilityId = "1";
+        Mockito.when(facilityRepository.find(facilityId)).thenReturn(null);
+        Mockito.when(facilityRegistryWrapper.find(facilityId)).thenReturn(null);
+        assertNotNull(facilityService.getCatchmentAreasByFacility(facilityId));
+        Mockito.verify(facilityRepository).find(facilityId);
+        Mockito.verify(facilityRegistryWrapper).find(facilityId);
+    }
+
+    private List<Catchment> getCatchmentsList(String catchments) {
+
+        List<String> catchmentsStringList = new ArrayList<>();
+
+        if(StringUtils.isNotBlank(catchments)) {
+            catchmentsStringList = Arrays.asList(catchments.split(","));
+        }
+
+        List<Catchment> catchmentArrayList = new ArrayList<>();
+
+        for (String catchment : catchmentsStringList) {
+            catchmentArrayList.add(new Catchment(catchment));
+        }
+
+        return catchmentArrayList;
     }
 }
