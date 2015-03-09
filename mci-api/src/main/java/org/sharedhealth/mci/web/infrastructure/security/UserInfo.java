6e18e68ee91c96388c0c05852296ae5469d50db5
Nehashri <nehashrl@thoughtworks.com>
2015-03-09 18:23:02 +0530

    Neha | ignoring unknown properties in User info.

--- mci-api/src/main/java/org/sharedhealth/mci/web/infrastructure/security/UserInfo.java
+++ mci-api/src/main/java/org/sharedhealth/mci/web/infrastructure/security/UserInfo.java
@@ -1,11 +1,13 @@
 package org.sharedhealth.mci.web.infrastructure.security;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserInfo {
     @JsonProperty("user")
     private String name;
