c18c501ad567c9a4ad39b99f4245112f13a19817
rnjn <runjan@gmail.com>
2015-03-18 10:43:02 +0530

    Removing unnecessary env var

--- mci-api/scripts/rpm/start.sh
+++ mci-api/scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java -Dserver.port=$MCI_PORT -jar $PROG_FILE > /var/log/mci/mci.log &
+nohup java -Dserver.port=$MCI_PORT -jar /opt/mci/lib/mci-api.war > /var/log/mci/mci.log &
 echo $! > /var/run/mci/mci.pid
