d0cb484d9cf2500ff16eb7dfe258d42f2d10aa25
Raju Mazumder <rajuniit@gmail.com>
2015-04-12 23:17:47 +0600

    Raju | BSHR-694 | Added initial delay for LR Sync

--- mci-api/src/main/java/org/sharedhealth/mci/web/tasks/LRSyncTask.java
+++ mci-api/src/main/java/org/sharedhealth/mci/web/tasks/LRSyncTask.java
@@ -33,37 +33,37 @@ public class LRSyncTask {
     @Autowired
     LocationDataSync locationDataSync;
 
-    @Scheduled(fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
+    @Scheduled(initialDelayString = "${LR_SYNC_INITIAL_DELAY}", fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
     public void syncDivision() throws IOException {
         logger.info("Division Syncing start....");
         locationDataSync.syncLRData(LR_DIVISION_URI_PATH, DIVISION_TYPE);
     }
 
-    @Scheduled(fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
+    @Scheduled(initialDelayString = "${LR_SYNC_INITIAL_DELAY}", fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
     public void syncDistrict() throws IOException {
         logger.info("District Syncing start....");
         locationDataSync.syncLRData(LR_DISTRICT_URI_PATH, DISTRICT_TYPE);
     }
 
-    @Scheduled(fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
+    @Scheduled(initialDelayString = "${LR_SYNC_INITIAL_DELAY}", fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
     public void syncUpazila() throws IOException {
         logger.info("Upazila Syncing start....");
         locationDataSync.syncLRData(LR_UPAZILA_URI_PATH, UPAZILA_TYPE);
     }
 
-    @Scheduled(fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
+    @Scheduled(initialDelayString = "${LR_SYNC_INITIAL_DELAY}", fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
     public void syncPaurasava() throws IOException {
         logger.info("Paurasava Syncing start....");
         locationDataSync.syncLRData(LR_PAURASAVA_PATH, PAURASAVA_TYPE);
     }
 
-    @Scheduled(fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
+    @Scheduled(initialDelayString = "${LR_SYNC_INITIAL_DELAY}", fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
     public void syncUnion() throws IOException {
         logger.info("Union Syncing start....");
         locationDataSync.syncLRData(LR_UNION_URI_PATH, UNION_TYPE);
     }
 
-    @Scheduled(fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
+    @Scheduled(initialDelayString = "${LR_SYNC_INITIAL_DELAY}", fixedDelayString = "${LR_SYNC_FIXED_DELAY}")
     public void syncWard() throws IOException {
         logger.info("Ward Syncing start....");
         locationDataSync.syncLRData(LR_WARD_URI_PATH, WARD_TYPE);
