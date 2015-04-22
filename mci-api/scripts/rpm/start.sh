a76bc628bfcb676196bb495395f410b8e49e2194
rnjn <runjan@gmail.com>
2015-04-22 12:19:19 +0530

    Fixing log-levels for MCI

--- mci-api/scripts/rpm/start.sh
+++ mci-api/scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java -Dserver.port=$MCI_PORT -DSHR_LOG_LEVEL=$MCI_LOG_LEVEL -jar  /opt/mci/lib/mci-api.war > /dev/null 2>&1  &
+nohup java -Dserver.port=$MCI_PORT -DMCI_LOG_LEVEL=$MCI_LOG_LEVEL -jar  /opt/mci/lib/mci-api.war > /dev/null 2>&1  &
 echo $! > /var/run/mci/mci.pid
