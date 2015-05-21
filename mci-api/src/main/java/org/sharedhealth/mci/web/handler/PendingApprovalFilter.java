6b920b5f501a68bb740cabea1842bbaa2ef1c284
Utsab Banerjee <utsabban@thoughtworks.com>
2015-05-21 17:20:10 +0530

    Utsab | BDSHR - 000 | using local variables in pending approval filter

--- mci-api/src/main/java/org/sharedhealth/mci/web/handler/PendingApprovalFilter.java
+++ mci-api/src/main/java/org/sharedhealth/mci/web/handler/PendingApprovalFilter.java
@@ -29,8 +29,6 @@ public class PendingApprovalFilter {
     private static final String NEEDS_APPROVAL = "NA";
     private static final String NON_UPDATABLE = "NU";
 
-    private PatientData newPatient;
-    private Requester requestedBy;
     private ApprovalFieldService properties;
 
     @Autowired
@@ -39,80 +37,80 @@ public class PendingApprovalFilter {
     }
 
     public PatientData filter(PatientData existingPatient, PatientData updateRequest) {
-        this.newPatient = new PatientData();
-        this.requestedBy = updateRequest.getRequester();
+        PatientData newPatient = new PatientData();
+        Requester requestedBy = updateRequest.getRequester();
         newPatient.setPendingApprovals(existingPatient.getPendingApprovals());
 
-        newPatient.setHealthId(processString(HID, existingPatient.getHealthId(), updateRequest.getHealthId(), this.requestedBy));
-        newPatient.setNationalId(processString(NID, existingPatient.getNationalId(), updateRequest.getNationalId(), this.requestedBy));
-        newPatient.setNameBangla(processString(NAME_BANGLA, existingPatient.getNameBangla(), updateRequest.getNameBangla(), this.requestedBy));
-        newPatient.setBirthRegistrationNumber(processString(BIN_BRN, existingPatient.getBirthRegistrationNumber(), updateRequest.getBirthRegistrationNumber(), this.requestedBy));
-        newPatient.setGivenName(processString(GIVEN_NAME, existingPatient.getGivenName(), updateRequest.getGivenName(), this.requestedBy));
-        newPatient.setSurName(processString(SUR_NAME, existingPatient.getSurName(), updateRequest.getSurName(), this.requestedBy));
-        newPatient.setDateOfBirth(processString(DATE_OF_BIRTH, existingPatient.getDateOfBirth(), updateRequest.getDateOfBirth(), this.requestedBy));
-        newPatient.setGender(processString(GENDER, existingPatient.getGender(), updateRequest.getGender(), this.requestedBy));
-        newPatient.setOccupation(processString(OCCUPATION, existingPatient.getOccupation(), updateRequest.getOccupation(), this.requestedBy));
-        newPatient.setEducationLevel(processString(EDU_LEVEL, existingPatient.getEducationLevel(), updateRequest.getEducationLevel(), this.requestedBy));
+        newPatient.setHealthId(processString(HID, existingPatient.getHealthId(), updateRequest.getHealthId(), requestedBy, newPatient));
+        newPatient.setNationalId(processString(NID, existingPatient.getNationalId(), updateRequest.getNationalId(), requestedBy, newPatient));
+        newPatient.setNameBangla(processString(NAME_BANGLA, existingPatient.getNameBangla(), updateRequest.getNameBangla(), requestedBy, newPatient));
+        newPatient.setBirthRegistrationNumber(processString(BIN_BRN, existingPatient.getBirthRegistrationNumber(), updateRequest.getBirthRegistrationNumber(), requestedBy, newPatient));
+        newPatient.setGivenName(processString(GIVEN_NAME, existingPatient.getGivenName(), updateRequest.getGivenName(), requestedBy, newPatient));
+        newPatient.setSurName(processString(SUR_NAME, existingPatient.getSurName(), updateRequest.getSurName(), requestedBy, newPatient));
+        newPatient.setDateOfBirth(processString(DATE_OF_BIRTH, existingPatient.getDateOfBirth(), updateRequest.getDateOfBirth(), requestedBy, newPatient));
+        newPatient.setGender(processString(GENDER, existingPatient.getGender(), updateRequest.getGender(), requestedBy, newPatient));
+        newPatient.setOccupation(processString(OCCUPATION, existingPatient.getOccupation(), updateRequest.getOccupation(), requestedBy, newPatient));
+        newPatient.setEducationLevel(processString(EDU_LEVEL, existingPatient.getEducationLevel(), updateRequest.getEducationLevel(), requestedBy, newPatient));
         newPatient.setRelations(updateRequest.getRelations()); //TODO : rewrite after relations bug is fixed.
-        newPatient.setUid(processString(UID, existingPatient.getUid(), updateRequest.getUid(), this.requestedBy));
-        newPatient.setPlaceOfBirth(processString(PLACE_OF_BIRTH, existingPatient.getPlaceOfBirth(), updateRequest.getPlaceOfBirth(), this.requestedBy));
-        newPatient.setReligion(processString(RELIGION, existingPatient.getReligion(), updateRequest.getReligion(), this.requestedBy));
-        newPatient.setBloodGroup(processString(BLOOD_GROUP, existingPatient.getBloodGroup(), updateRequest.getBloodGroup(), this.requestedBy));
-        newPatient.setNationality(processString(NATIONALITY, existingPatient.getNationality(), updateRequest.getNationality(), this.requestedBy));
-        newPatient.setDisability(processString(DISABILITY, existingPatient.getDisability(), updateRequest.getDisability(), this.requestedBy));
-        newPatient.setEthnicity(processString(ETHNICITY, existingPatient.getEthnicity(), updateRequest.getEthnicity(), this.requestedBy));
-        newPatient.setPrimaryContact(processString(PRIMARY_CONTACT, existingPatient.getPrimaryContact(), updateRequest.getPrimaryContact(), this.requestedBy));
-        newPatient.setMaritalStatus(processString(MARITAL_STATUS, existingPatient.getMaritalStatus(), updateRequest.getMaritalStatus(), this.requestedBy));
-        newPatient.setConfidential(processString(CONFIDENTIAL, existingPatient.getConfidential(), updateRequest.getConfidential(), this.requestedBy));
-        newPatient.setCreatedAt(processUuid(CREATED, existingPatient.getCreatedAt(), updateRequest.getCreatedAt(), this.requestedBy));
-        newPatient.setUpdatedAt(processUuid(MODIFIED, existingPatient.getUpdatedAt(), updateRequest.getUpdatedAt(), this.requestedBy));
-        newPatient.setPhoneNumber(processPhoneNumber(PHONE_NUMBER, existingPatient.getPhoneNumber(), updateRequest.getPhoneNumber(), this.requestedBy));
-        newPatient.setPatientStatus(processPatientStatus(STATUS, existingPatient.getPatientStatus(), updateRequest.getPatientStatus(), this.requestedBy));
-        newPatient.setPrimaryContactNumber(processPhoneNumber(PRIMARY_CONTACT_NUMBER, existingPatient.getPrimaryContactNumber(), updateRequest.getPrimaryContactNumber(), this.requestedBy));
-        newPatient.setAddress(processAddress(PRESENT_ADDRESS, existingPatient.getAddress(), updateRequest.getAddress(), this.requestedBy));
-        newPatient.setPermanentAddress(processAddress(PERMANENT_ADDRESS, existingPatient.getPermanentAddress(), updateRequest.getPermanentAddress(), this.requestedBy));
-        newPatient.setHouseholdCode(processString(HOUSEHOLD_CODE, existingPatient.getHouseholdCode(), updateRequest.getHouseholdCode(), this.requestedBy));
-        newPatient.setActive((Boolean) process(ACTIVE, existingPatient.getActive(), updateRequest.getActive(), this.requestedBy));
-        newPatient.setMergedWith(processString(MERGED_WITH, existingPatient.getMergedWith(), updateRequest.getMergedWith(), this.requestedBy));
+        newPatient.setUid(processString(UID, existingPatient.getUid(), updateRequest.getUid(), requestedBy, newPatient));
+        newPatient.setPlaceOfBirth(processString(PLACE_OF_BIRTH, existingPatient.getPlaceOfBirth(), updateRequest.getPlaceOfBirth(), requestedBy, newPatient));
+        newPatient.setReligion(processString(RELIGION, existingPatient.getReligion(), updateRequest.getReligion(), requestedBy, newPatient));
+        newPatient.setBloodGroup(processString(BLOOD_GROUP, existingPatient.getBloodGroup(), updateRequest.getBloodGroup(), requestedBy, newPatient));
+        newPatient.setNationality(processString(NATIONALITY, existingPatient.getNationality(), updateRequest.getNationality(), requestedBy, newPatient));
+        newPatient.setDisability(processString(DISABILITY, existingPatient.getDisability(), updateRequest.getDisability(), requestedBy, newPatient));
+        newPatient.setEthnicity(processString(ETHNICITY, existingPatient.getEthnicity(), updateRequest.getEthnicity(), requestedBy, newPatient));
+        newPatient.setPrimaryContact(processString(PRIMARY_CONTACT, existingPatient.getPrimaryContact(), updateRequest.getPrimaryContact(), requestedBy, newPatient));
+        newPatient.setMaritalStatus(processString(MARITAL_STATUS, existingPatient.getMaritalStatus(), updateRequest.getMaritalStatus(), requestedBy, newPatient));
+        newPatient.setConfidential(processString(CONFIDENTIAL, existingPatient.getConfidential(), updateRequest.getConfidential(), requestedBy, newPatient));
+        newPatient.setCreatedAt(processUuid(CREATED, existingPatient.getCreatedAt(), updateRequest.getCreatedAt(), requestedBy, newPatient));
+        newPatient.setUpdatedAt(processUuid(MODIFIED, existingPatient.getUpdatedAt(), updateRequest.getUpdatedAt(), requestedBy, newPatient));
+        newPatient.setPhoneNumber(processPhoneNumber(PHONE_NUMBER, existingPatient.getPhoneNumber(), updateRequest.getPhoneNumber(), requestedBy, newPatient));
+        newPatient.setPatientStatus(processPatientStatus(STATUS, existingPatient.getPatientStatus(), updateRequest.getPatientStatus(), requestedBy, newPatient));
+        newPatient.setPrimaryContactNumber(processPhoneNumber(PRIMARY_CONTACT_NUMBER, existingPatient.getPrimaryContactNumber(), updateRequest.getPrimaryContactNumber(), requestedBy, newPatient));
+        newPatient.setAddress(processAddress(PRESENT_ADDRESS, existingPatient.getAddress(), updateRequest.getAddress(), requestedBy, newPatient));
+        newPatient.setPermanentAddress(processAddress(PERMANENT_ADDRESS, existingPatient.getPermanentAddress(), updateRequest.getPermanentAddress(), requestedBy, newPatient));
+        newPatient.setHouseholdCode(processString(HOUSEHOLD_CODE, existingPatient.getHouseholdCode(), updateRequest.getHouseholdCode(), requestedBy, newPatient));
+        newPatient.setActive((Boolean) process(ACTIVE, existingPatient.getActive(), updateRequest.getActive(), requestedBy, newPatient));
+        newPatient.setMergedWith(processString(MERGED_WITH, existingPatient.getMergedWith(), updateRequest.getMergedWith(), requestedBy, newPatient));
 
         return newPatient;
     }
 
-    private PhoneNumber processPhoneNumber(String key, PhoneNumber oldValue, PhoneNumber newValue, Requester requester) {
-        Object phoneNumber = process(key, oldValue, newValue,requester);
+    private PhoneNumber processPhoneNumber(String key, PhoneNumber oldValue, PhoneNumber newValue, Requester requester, PatientData newPatient) {
+        Object phoneNumber = process(key, oldValue, newValue, requester, newPatient);
         return phoneNumber == null ? null : (PhoneNumber) phoneNumber;
     }
 
-    private Address processAddress(String key, Address oldValue, Address newValue, Requester requester) {
+    private Address processAddress(String key, Address oldValue, Address newValue, Requester requester, PatientData newPatient) {
 
         if (newValue != null && !newValue.isEmpty()) {
             newValue.setCountryCode(COUNTRY_CODE_BANGLADESH);
         }
 
-        Object address = process(key, oldValue, newValue,requester);
+        Object address = process(key, oldValue, newValue, requester, newPatient);
         return address == null ? null : (Address) address;
     }
 
-    private PatientStatus processPatientStatus(String key, PatientStatus oldValue, PatientStatus newValue, Requester requester) {
-        Object patientStatus = process(key, oldValue, newValue, requester);
+    private PatientStatus processPatientStatus(String key, PatientStatus oldValue, PatientStatus newValue, Requester requester, PatientData newPatient) {
+        Object patientStatus = process(key, oldValue, newValue, requester, newPatient);
         return patientStatus == null ? null : (PatientStatus) patientStatus;
     }
 
-    private UUID processUuid(String key, UUID oldValue, UUID newValue, Requester requester) {
-        Object value = process(key, oldValue, newValue, requester);
+    private UUID processUuid(String key, UUID oldValue, UUID newValue, Requester requester, PatientData newPatient) {
+        Object value = process(key, oldValue, newValue, requester, newPatient);
         return value == null ? null : (UUID) value;
     }
 
-    private String processString(String key, String oldValue, String newValue, Requester requester) {
+    private String processString(String key, String oldValue, String newValue, Requester requester, PatientData newPatient) {
         if ("".equals(trim(newValue))) {
             oldValue = defaultString(oldValue);
             newValue = defaultString(newValue);
         }
-        Object value = process(key, oldValue, newValue, requester);
+        Object value = process(key, oldValue, newValue, requester, newPatient);
         return value == null ? null : valueOf(value);
     }
 
-    private Object process(String key, Object oldValue, Object newValue, Requester requester) {
+    private Object process(String key, Object oldValue, Object newValue, Requester requester, PatientData newPatient) {
         if (newValue == null) {
             return oldValue;
         }
@@ -130,21 +128,21 @@ public class PendingApprovalFilter {
             }
 
             if (property.equals(NEEDS_APPROVAL) && !newValue.equals(oldValue)) {
-                newPatient.addPendingApproval(buildPendingApproval(key, newValue));
+                newPatient.addPendingApproval(buildPendingApproval(key, newValue, requester));
                 return oldValue;
             }
         }
         return newValue;
     }
 
-    private PendingApproval buildPendingApproval(String key, Object newValue) {
+    private PendingApproval buildPendingApproval(String key, Object newValue, Requester requester) {
         PendingApproval pendingApproval = new PendingApproval();
         pendingApproval.setName(key);
 
         TreeMap<UUID, PendingApprovalFieldDetails> fieldDetailsMap = new TreeMap<>();
         PendingApprovalFieldDetails fieldDetails = new PendingApprovalFieldDetails();
         fieldDetails.setValue(newValue);
-        fieldDetails.setRequestedBy(this.requestedBy);
+        fieldDetails.setRequestedBy(requester);
         UUID uuid = timeBased();
         fieldDetails.setCreatedAt(unixTimestamp(uuid));
         fieldDetailsMap.put(uuid, fieldDetails);
