653c4c1bffb4be39e9e5e9992e55fa078cc03892
Soham <sohamgh@thoughtworks.com>
2015-06-17 20:49:25 +0530

    Soham | BSHR-553 | Dedup scheduler.

---
+++ mci-api/src/test/java/org/sharedhealth/mci/tasks/DuplicatePatientFeedTaskTest.java
@@ -0,0 +1,26 @@
+package org.sharedhealth.mci.tasks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.sharedhealth.mci.web.service.DuplicatePatientFeedService;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class DuplicatePatientFeedTaskTest {
+
+    @Mock
+    private DuplicatePatientFeedService duplicatePatientFeedService;
+
+    @Before
+    public void setup() throws Exception {
+        initMocks(this);
+    }
+
+    @Test
+    public void shouldProcessDuplicatePatientFeed() throws Exception {
+        new DuplicatePatientFeedTask(duplicatePatientFeedService).execute();
+        verify(duplicatePatientFeedService).processDuplicatePatients();
+    }
+}
