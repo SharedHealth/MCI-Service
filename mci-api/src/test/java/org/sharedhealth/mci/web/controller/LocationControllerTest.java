409aac0bace3acc9837989e67b1e76fbbc54532a
Soham <sohamgh@thoughtworks.com>
2015-06-23 16:07:40 +0530

    Utsab, Soham | BSHR-617 | Location controller IT converted to unit test.

--- mci-api/src/test/java/org/sharedhealth/mci/web/controller/LocationControllerTest.java
+++ mci-api/src/test/java/org/sharedhealth/mci/web/controller/LocationControllerTest.java
@@ -1,131 +1,109 @@
 package org.sharedhealth.mci.web.controller;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.sharedhealth.mci.web.handler.MCIMultiResponse;
 import org.sharedhealth.mci.web.infrastructure.security.TokenAuthentication;
 import org.sharedhealth.mci.web.infrastructure.security.UserInfo;
 import org.sharedhealth.mci.web.infrastructure.security.UserProfile;
 import org.sharedhealth.mci.web.mapper.LocationCriteria;
 import org.sharedhealth.mci.web.mapper.LocationData;
 import org.sharedhealth.mci.web.service.LocationService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
-import static org.springframework.http.HttpStatus.OK;
+import static org.sharedhealth.mci.web.infrastructure.security.UserInfo.MCI_USER_GROUP;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LocationControllerTest {
 
+    private static final String USER_INFO_FACILITY = "100067";
+    private static final String API_END_POINT = "/locations";
+
     @Mock
     private LocationService locationService;
-
     @Mock
     private LocalValidatorFactoryBean localValidatorFactoryBean;
+    @Mock
+    private SecurityContext securityContext;
 
     private MockMvc mockMvc;
-    public static final String API_END_POINT = "/locations";
-    private LocationCriteria locationCriteria;
-    private List<LocationData> locations;
 
     @Before
-    public void setup() throws ParseException {
+    public void setup() {
         initMocks(this);
         mockMvc = MockMvcBuilders
                 .standaloneSetup(new LocationController(locationService))
-                .setValidator(validator())
+                .setValidator(localValidatorFactoryBean)
                 .build();
-
-        locationCriteria = new LocationCriteria();
         SecurityContextHolder.getContext().setAuthentication(new TokenAuthentication(getUserInfo(), true));
-
     }
 
-    private UserInfo getUserInfo() {UserProfile userProfile = new UserProfile("facility", "100067", null);
-
-        return new UserInfo("102", "ABC", "abc@mail", 1, true, "111100",
-                new ArrayList<String>(), asList(userProfile));
-    }
-
-
-    @Test
-    public void shouldFindAllDivisionsIfLevelAbsent() throws Exception {
-
-        buildLocationData();
-        when(locationService.findLocationsByParent(locationCriteria)).thenReturn(this.locations);
-
-        HashMap<String, String> additionalInfo = new HashMap<>();
-
-        MCIMultiResponse mciMultiResponse = new MCIMultiResponse(locations, additionalInfo, OK);
-
-        mockMvc.perform(get(API_END_POINT))
-                .andExpect(request().asyncResult(new ResponseEntity<>(mciMultiResponse, mciMultiResponse.httpStatusObject)));
-
-        verify(locationService).findLocationsByParent(locationCriteria);
-
+    private UserInfo getUserInfo() {
+        UserProfile userProfile = new UserProfile("facility", USER_INFO_FACILITY, null);
+        return new UserInfo("102", "ABC", "abc@mail", 1, true, "111100", asList(MCI_USER_GROUP), asList(userProfile));
     }
 
     @Test
-    public void shouldFindLocationsByLevel() throws Exception {
+    public void shouldFindLocationByParent() throws Exception {
+        List<LocationData> locationDataList = buildLocationData();
 
-        List<LocationData> locations = new ArrayList<>();
-        buildLocationData();
-        locationCriteria.setParent("10");
-        when(locationService.findLocationsByParent(locationCriteria)).thenReturn(locations);
+        when(locationService.findLocationsByParent(new LocationCriteria())).thenReturn(locationDataList);
+        MvcResult result = mockMvc.perform(get(API_END_POINT))
+                .andExpect(request().asyncStarted())
+                .andReturn();
 
-        HashMap<String, String> additionalInfo = new HashMap<>();
+        mockMvc.perform(asyncDispatch(result))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.http_status", is(200)))
 
-        MCIMultiResponse mciMultiResponse = new MCIMultiResponse(locations, additionalInfo, OK);
+                .andExpect(jsonPath("$results[0].id", is(nullValue())))
+                .andExpect(jsonPath("$results[0].type", is(nullValue())))
+                .andExpect(jsonPath("$results[0].active", is("1")))
+                .andExpect(jsonPath("$results[0].updatedAt", is(nullValue())))
+                .andExpect(jsonPath("$results[0].code", is("10")))
+                .andExpect(jsonPath("$results[0].name", is("Barisal")))
 
-        mockMvc.perform(get(API_END_POINT + "?parent=10"))
-                .andExpect(request().asyncResult(new ResponseEntity<>(mciMultiResponse, mciMultiResponse.httpStatusObject)));
+                .andExpect(jsonPath("$results[1].id", is(nullValue())))
+                .andExpect(jsonPath("$results[1].type", is(nullValue())))
+                .andExpect(jsonPath("$results[1].active", is("1")))
+                .andExpect(jsonPath("$results[1].updatedAt", is(nullValue())))
+                .andExpect(jsonPath("$results[1].code", is("20")))
+                .andExpect(jsonPath("$results[1].name", is("Chittagong")))
 
-        verify(locationService).findLocationsByParent(locationCriteria);
-
+                .andExpect(jsonPath("$results[2].id").doesNotExist());
     }
 
-    private LocalValidatorFactoryBean validator() {
-        return localValidatorFactoryBean;
+    private List<LocationData> buildLocationData() {
+        List<LocationData> locationDataList = new ArrayList<>();
+        LocationData locationData = new LocationData();
+        locationData.setName("Barisal");
+        locationData.setCode("10");
+        locationData.setActive("1");
+        locationDataList.add(locationData);
+
+        locationData = new LocationData();
+        locationData.setName("Chittagong");
+        locationData.setCode("20");
+        locationData.setActive("1");
+        locationDataList.add(locationData);
+        return locationDataList;
     }
-
-    private List<LocationData> buildLocationData()
-    {
-        locations = new ArrayList<>();
-
-        LocationData data = new LocationData();
-        data.setCode("10");
-        data.setName("Dhaka");
-        data.setParent("00");
-
-        LocationData data1 = new LocationData();
-        data.setCode("10");
-        data.setName("Dhaka");
-        data.setParent("00");
-
-        locations.add(data);
-        locations.add(data1);
-
-        return locations;
-    }
-
-
 }
-
