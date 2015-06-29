8c42ccdcbaabe900001b516eaaf6681097fdb8f4
Utsab Banerjee <utsabban@thoughtworks.com>
2015-06-29 11:50:00 +0530

    Utsab, Soham | fixing builds

--- mci-api/src/test/java/org/sharedhealth/mci/web/service/HealthIdServiceIT.java
+++ mci-api/src/test/java/org/sharedhealth/mci/web/service/HealthIdServiceIT.java
@@ -59,7 +59,7 @@ public class HealthIdServiceIT {
 
     @Test
     public void shouldGenerateUniqueBlock() throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(100);
+        ExecutorService executor = Executors.newFixedThreadPool(50);
         final Set<Future<List<HealthId>>> eventualHealthIds = new HashSet<>();
         for (int i = 0; i < 100; i++) {
             Callable<List<HealthId>> nextBlock = new Callable<List<HealthId>>() {
